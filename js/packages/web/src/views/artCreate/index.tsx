4d6f8e9c70626ea5ed5aec0213e7ac8a482d08fd
nikhil arora <narora200@gmail.com>
2022-01-26 11:32:19 +0530

    refactor: undo prettier changes

--- js/packages/web/src/views/artCreate/index.tsx
+++ js/packages/web/src/views/artCreate/index.tsx
@@ -75,9 +75,8 @@ export const ArtCreateView = () => {
   const [step, setStep] = useState<number>(0);
   const [stepsVisible, setStepsVisible] = useState<boolean>(true);
   const [isMinting, setMinting] = useState<boolean>(false);
-  const [nft, setNft] = useState<
-    { metadataAccount: StringPublicKey } | undefined
-  >(undefined);
+  const [nft, setNft] =
+    useState<{ metadataAccount: StringPublicKey } | undefined>(undefined);
   const [files, setFiles] = useState<File[]>([]);
   const [attributes, setAttributes] = useState<IMetadataExtension>({
     name: 'cryptoisreal. <id>',
@@ -253,11 +252,7 @@ const CategoryStep = (props: {
         <h2>Create a new item</h2>
         <p>
           First time creating on Metaplex?{' '}
-          <a
-            href="https://docs.metaplex.com/create-store/sell"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://docs.metaplex.com/create-store/sell" target="_blank" rel="noreferrer">
             Read our creators’ guide.
           </a>
         </p>
@@ -568,11 +563,8 @@ const UploadStep = (props: {
                   : mainFile && mainFile.name,
             });
             const url = await fetch(customURL).then(res => res.blob());
-            const files = [
-              coverFile,
-              mainFile,
-              customURL ? new File([url], customURL) : '',
-            ].filter(f => f) as File[];
+            const files = [coverFile, mainFile, customURL ? new File([url], customURL) : '']
+              .filter(f => f) as File[];
 
             props.setFiles(files);
             props.confirm();
@@ -754,49 +746,47 @@ const InfoStep = (props: {
           </label>
           <Form name="dynamic_attributes" form={form} autoComplete="off">
             <Form.List name="attributes" initialValue={defaultAttributes}>
-              {(fields, { add, remove }) => {
-                return (
-                  <>
-                    {fields.map(({ key, name, fieldKey }) => (
-                      <Space key={key} align="baseline">
-                        <Form.Item
-                          name={[name, 'trait_type']}
-                          fieldKey={[fieldKey, 'trait_type']}
-                          hasFeedback
-                        >
-                          <Input placeholder="trait_type (Optional)" />
-                        </Form.Item>
-                        <Form.Item
-                          name={[name, 'value']}
-                          fieldKey={[fieldKey, 'value']}
-                          rules={[{ required: true, message: 'Missing value' }]}
-                          hasFeedback
-                        >
-                          <Input placeholder="value" />
-                        </Form.Item>
-                        <Form.Item
-                          name={[name, 'display_type']}
-                          fieldKey={[fieldKey, 'display_type']}
-                          hasFeedback
-                        >
-                          <Input placeholder="display_type (Optional)" />
-                        </Form.Item>
-                        <MinusCircleOutlined onClick={() => remove(name)} />
-                      </Space>
-                    ))}
-                    <Form.Item>
-                      <Button
-                        type="dashed"
-                        onClick={() => add()}
-                        block
-                        icon={<PlusOutlined />}
+              {(fields, { add, remove }) => (
+                <>
+                  {fields.map(({ key, name, fieldKey }) => (
+                    <Space key={key} align="baseline">
+                      <Form.Item
+                        name={[name, 'trait_type']}
+                        fieldKey={[fieldKey, 'trait_type']}
+                        hasFeedback
                       >
-                        Add attribute
-                      </Button>
-                    </Form.Item>
-                  </>
-                );
-              }}
+                        <Input placeholder="trait_type (Optional)" />
+                      </Form.Item>
+                      <Form.Item
+                        name={[name, 'value']}
+                        fieldKey={[fieldKey, 'value']}
+                        rules={[{ required: true, message: 'Missing value' }]}
+                        hasFeedback
+                      >
+                        <Input placeholder="value" />
+                      </Form.Item>
+                      <Form.Item
+                        name={[name, 'display_type']}
+                        fieldKey={[fieldKey, 'display_type']}
+                        hasFeedback
+                      >
+                        <Input placeholder="display_type (Optional)" />
+                      </Form.Item>
+                      <MinusCircleOutlined onClick={() => remove(name)} />
+                    </Space>
+                  ))}
+                  <Form.Item>
+                    <Button
+                      type="dashed"
+                      onClick={() => add()}
+                      block
+                      icon={<PlusOutlined />}
+                    >
+                      Add attribute
+                    </Button>
+                  </Form.Item>
+                </>
+              )}
             </Form.List>
           </Form>
         </Col>
